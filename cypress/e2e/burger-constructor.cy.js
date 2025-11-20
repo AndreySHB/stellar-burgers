5bbf99393048a46bb0c769942d975f37b2089018
AndreySHB <deerhunter9136@gmail.com>
2025-11-20 11:30:55 +0300

    cypress tests fix

--- cypress/e2e/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor.cy.js
@@ -25,6 +25,12 @@ describe('Burger Constructor', () => {
     cy.wait('@getIngredients');
   });
 
+  afterEach(() => {
+    //Сбрасываем токены авторизации
+    cy.clearCookie('accessToken');
+    window.localStorage.removeItem('refreshToken');
+  });
+
   it('should load ingredients and display them', () => {
     // Проверяем, что ингредиенты загрузились
     cy.contains('Краторная булка N-200i').should('exist');
@@ -58,7 +64,8 @@ describe('Burger Constructor', () => {
     cy.contains('Биокотлета из марсианской Магнолии').should('exist');
   });
 
-  it('should open and close ingredient modal', () => {
+  // проверка закрытия модального окна по клику на кнопку esc
+  it('should open and close ingredient modal by esc button', () => {
     // Кликаем на ингредиент
     cy.contains('Краторная булка N-200i').click();
 
@@ -68,6 +75,26 @@ describe('Burger Constructor', () => {
 
     // Закрываем через ESC (работает всегда)
     cy.get('body').type('{esc}');
+
+    // Проверяем что модальное закрылось
+    cy.contains('Детали ингредиента').should('not.exist');
+    cy.contains('Калории, ккал').should('not.exist');
+  });
+
+  // проверка закрытия модального окна по клику на крестик
+  it('should open and close ingredient modal by close button', () => {
+    // Кликаем на ингредиент
+    cy.contains('Краторная булка N-200i').click();
+
+    // Проверяем что модальное открылось по наличию контента
+    cy.contains('Детали ингредиента').should('be.visible');
+    cy.contains('Калории, ккал').should('be.visible');
+
+    // Закрываем через крестик
+    cy.contains('Детали ингредиента')
+      .parent() // переходим к заголовку
+      .find('button') // находим кнопку в заголовке
+      .click();
 
     // Проверяем что модальное закрылось
     cy.contains('Детали ингредиента').should('not.exist');
@@ -91,5 +118,37 @@ describe('Burger Constructor', () => {
 
     // Проверяем, что кнопка стала активной
     cy.contains('Оформить заказ').should('not.be.disabled');
+
+    // Кликаем на кнопку "Оформить заказ"
+    cy.contains('Оформить заказ').click();
+
+    // Заполняем форму логина
+    cy.get('input[name="email"]').type('test@test');
+    cy.get('input[name="password"]').type('test');
+    cy.get('button[type="submit"]').click();
+
+    cy.wait('@login');
+
+    // Кликаем на кнопку "Оформить заказ" еще раз
+    cy.contains('Оформить заказ').click();
+
+    // Проверяем, что открылось модальное окно с номером заказа
+    cy.contains('идентификатор заказа', { timeout: 1000 }).should('exist');
+    cy.contains('12345').should('exist');
+    cy.contains('Ваш заказ начали готовить').should('exist');
+
+    // Закрываем модальное окно заказа
+    cy.get('body').type('{esc}');
+
+    // Проверяем, что модальное окно закрылось
+    cy.contains('идентификатор заказа').should('not.exist');
+
+    // Проверяем, что конструктор очистился после создания заказа
+    cy.contains('Выберите булки').should('exist');
+    cy.contains('Выберите начинку').should('exist');
+
+    // Проверяем, что добавленные ингредиенты исчезли из конструктора
+    cy.contains('Краторная булка N-200i (верх)').should('not.exist');
+    cy.contains('Краторная булка N-200i (низ)').should('not.exist');
   });
 });
